5146751b360af6b04b76684c5e449b4e6b473db4
sungsungsungg <137283602+sungsungsungg@users.noreply.github.com>
2025-02-07 16:41:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,5 @@
 "use strict";
 
-// const element = document.getElementById("box");
-
-// element.scrollIntoView();
-// element.scrollIntoView(false);
-// element.scrollIntoView({ block: "end" });
-// element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-
-// const goToTop = document.getElementById("go-to-top");
-// const goToBottom = document.getElementById("go-to-bottom");
-
-// goToBottom.addEventListener("click", () => {
-//   goToTop.scrollIntoView({ behavior: "instant", block: "end" });
-// });
-
-//Section
-const home = document.querySelector("#home");
-const about = document.querySelector("#about");
-const skills = document.querySelector("#skills");
-const portfolio = document.querySelector("#portfolio");
-const contact = document.querySelector("#contact");
-
-//navButtons
-const goToHome = document.getElementById("home-button");
-const goToAbout = document.getElementById("about-button");
-const goToSkills = document.getElementById("skills-button");
-const goToPortfolio = document.getElementById("portfolio-button");
-const goToContact = document.getElementById("contact-button");
-
-goToHome.addEventListener("click", () => {
-  home.scrollTo(0, 0);
-});
-
-goToAbout.addEventListener("click", () => {
-  about.scrollIntoView({ behavior: "smooth" });
-});
-
-goToSkills.addEventListener("click", () => {
-  skills.scrollIntoView({ behavior: "smooth" });
-});
-goToPortfolio.addEventListener("click", () => {
-  portfolio.scrollIntoView({ behavior: "smooth" });
-});
-goToContact.addEventListener("click", () => {
-  contact.scrollIntoView({ behavior: "smooth" });
-});
-
-//hi
-
 const modal = document.querySelectorAll(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelectorAll(".close-modal");
